71d49d8b2f6072d97a69c34d268cae48e3e5fc07
Luis H <goldff36@gmail.com>
2021-02-17 16:41:02 -0300

    Update index.py

--- index.py
+++ index.py
@@ -4,3 +4,6 @@ img= Image.open("Images/nomeDaImagem.jpg")
 blackAndWhite = img.convert("L")
 blackAndWhite.save('Images/nomeDaImagem.jpeg')
 blackAndWhite.show()
+
+
+#To use this code it is necessary to have Python and Pillow installed.
